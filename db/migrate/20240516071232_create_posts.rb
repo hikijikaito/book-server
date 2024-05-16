57f74c176f121d5983ba2f628a7495fc562a7565
hikijikaito <kaito.happy.19971121@gmail.com>
2024-05-16 16:27:38 +0900

    マイグレーションファイルの作成

--- db/migrate/20240516071232_create_posts.rb
+++ db/migrate/20240516071232_create_posts.rb
@@ -1,6 +1,14 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
+      t.string :title
+      t.date :date
+      t.text :synopsis
+      t.string :author
+      t.string :publisher
+      t.text :thoughts
+      t.integer :evaluation_id
+      t.string :tag, null: true, default: '' # タグのデフォルト値を空文字列に設定
 
       t.timestamps
     end
